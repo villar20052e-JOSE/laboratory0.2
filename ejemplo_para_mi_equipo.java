b8f54a31c6d0ae19f7ac5b79be848db7c0a3b82b
JOSE_VILLAR <villar20052@gmail.com>
2026-02-24 18:12:54 -0500

    ejmplo de como usar los comandos git

--- ejemplo_para_mi_equipo.java
+++ ejemplo_para_mi_equipo.java
@@ -1,4 +1,4 @@
-public class nombredelprograma {
+public class ejemplo_para_mi_equipo {
   public static void main(String[] args) {
     System.out.println("¡Hola, mundo!");
     System.out.println("ejemplo de programa");
