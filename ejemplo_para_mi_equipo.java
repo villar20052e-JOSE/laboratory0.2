13f60a1f8e07c90690cfe2c7ac2db5c449b3f711
JOSE_VILLAR <villar20052@gmail.com>
2026-02-26 17:11:53 -0500

    guia de comandos git

--- ejemplo_para_mi_equipo.java
+++ ejemplo_para_mi_equipo.java
@@ -1,6 +1,8 @@
 public class ejemplo_para_mi_equipo {
   public static void main(String[] args) {
-    System.out.println("¡Hola, mundo!");
-    System.out.println("ejemplo de programa");
+    System.out.println("git add . ( prepara los cambios para subir)");
+    System.out.println("git commit -am ( agragar comentario )");
+    System.out.println("git push (subir programa)");
+    System.out.println("git pull (descarga el progreso de tus compañeros)");
   }
 }
