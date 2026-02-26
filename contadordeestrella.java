d2ccf8b61eafedbd99d1a6448bf2c632161b2b97
kathia2908 <kathiacedeno191@gmail.com>
2026-02-25 21:05:16 -0500

    codigo de contador de estrella, incompleto

---
+++ contadordeestrella.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class contadordeestrella {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("¿Ingresa un numero para mostrar estrelas"?);
+        int numero = sc.nextInt();
+
+
+        for (int i = 1; <= numero; i++) {
+            System.out.print("*");
+
+        
+        }
+
+        sc. close();
+    }
+}
