ccbea5f9a2212869d4d97b179cf920e019601772
JOSE_VILLAR <villar20052@gmail.com>
2026-02-26 19:24:14 -0500

    este es mi programa de menu de comida, este programa fue hecho utilizando Scanner para la lectura y guarda de info del usuario(de lo que vaya pidiendo de comer), se utilizo tambien el switch para la toma de deciciones dependiendo de lo que el usuario elija, aplico un bucle con el do while para continuidad del programa

--- foodmenu.java
+++ foodmenu.java
@@ -5,156 +5,164 @@ public class FoodMenu {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
 
-    // Variables para la factura
-    String facturaPlato = "";
-    String facturaBebida = "";
-    String facturaExtra = "";
-    double total = 0;
+    // Variable para controlar si el usuario quiere seguir comprando
+    boolean continuar = true;
 
-    System.out.println("============================================");
-    System.out.println("      ¡BIENVENIDO AL GARAJE DEL SABOR!      ");
-    System.out.println("============================================\n");
+    // EL BUCLE DO-WHILE HACE QUE EL PROGRAMA SEA DINÁMICO
+    do {
 
-    // 1. PLATO PRINCIPAL
-    System.out.println("--- MENÚ DE PLATOS PRINCIPALES ---");
-    System.out.println("1. Hamburguesa");
-    System.out.println("2. Pizza");
-    System.out.println("3. Salchipapas ($6.50)");
-    System.out.println("4. Hot Dog ($2.00)");
-    System.out.println("5. Alitas BBQ ($8.50)");
-    System.out.println("6. Pollo Apanado ($3.50)");
-    System.out.println("7. Pollo Asado ($5.00)");
-    System.out.print("Seleccione su plato: ");
-    int plato = sc.nextInt();
+      // Variables para la factura
+      String facturaPlato = "";
+      String facturaBebida = "";
+      String facturaExtra = "";
+      double total = 0;
 
-    switch (plato) {
-      case 1: // HAMBURGUESA
-        System.out.print("¿De (1) Carne o (2) Pollo?: ");
-        int tipoH = sc.nextInt();
-        System.out.print("¿(1) Normal ($6.00) o (2) Doble Carne ($8.50)?: ");
-        int tamanoH = sc.nextInt();
-        facturaPlato = (tipoH == 1 ? "Hamburguesa de Carne" : "Hamburguesa de Pollo");
-        if (tamanoH == 2) {
-          facturaPlato += " Doble";
+      System.out.println("============================================");
+      System.out.println("      ¡BIENVENIDO AL GARAJE DEL SABOR!      ");
+      System.out.println("============================================\n");
+
+      // 1. PLATO PRINCIPAL
+      System.out.println("--- MENÚ DE PLATOS PRINCIPALES ---");
+      System.out.println("1. Hamburguesa");
+      System.out.println("2. Pizza");
+      System.out.println("3. Salchipapas ($6.50)");
+      System.out.println("4. Hot Dog ($2.00)");
+      System.out.println("5. Alitas BBQ ($8.50)");
+      System.out.println("6. Pollo Apanado ($3.50)");
+      System.out.println("7. Pollo Asado ($5.00)");
+      System.out.print("Seleccione su plato: ");
+      int plato = sc.nextInt();
+
+      switch (plato) {
+        case 1: // HAMBURGUESA
+          System.out.print("¿De (1) Carne o (2) Pollo?: ");
+          int tipoH = sc.nextInt();
+          System.out.print("¿(1) Normal ($6.00) o (2) Doble Carne ($8.50)?: ");
+          int tamanoH = sc.nextInt();
+          facturaPlato = (tipoH == 1 ? "Hamburguesa de Carne" : "Hamburguesa de Pollo");
+          if (tamanoH == 2) {
+            facturaPlato += " Doble";
+            total += 8.50;
+          } else {
+            total += 6.00;
+          }
+          break;
+
+        case 2: // PIZZA
+          System.out.println("Sabores: (1) Peperoni ($5.00), (2) Queso ($3.50), (3) Combinación ($8.50)");
+          int saborP = sc.nextInt();
+          switch (saborP) {
+            case 1:
+              facturaPlato = "Pizza Peperoni";
+              total += 5.00;
+              break;
+            case 2:
+              facturaPlato = "Pizza Queso";
+              total += 3.50;
+              break;
+            default:
+              facturaPlato = "Pizza Combinación";
+              // total += 8.50
+              break;
+          }
+          break;
+
+        case 3:
+          facturaPlato = "Salchipapas";
+          total += 6.50;
+          break;
+        case 4:
+          facturaPlato = "Hot Dog";
+          total += 2.00;
+          break;
+        case 5:
+          facturaPlato = "Alitas BBQ";
           total += 8.50;
-        } else {
-          total += 6.00;
-        }
-        break;
+          break;
+        case 6:
+          facturaPlato = "Pollo Apanado";
+          total += 3.50;
+          break;
+        case 7:
+          facturaPlato = "Pollo Asado";
+          total += 5.00;
+          break;
+        default:
+          facturaPlato = "Orden Genérica";
+          total += 5.00;
+          break;
+      }
 
-      case 2: // PIZZA
-        System.out.println("Sabores: (1) Peperoni ($5.00), (2) Queso ($3.50), (3) Combinación ($8.50)");
-        int saborP = sc.nextInt();
-        switch (saborP) {
-          case 1:
-            facturaPlato = "Pizza Peperoni";
-            total += 5.00;
-            break;
-          case 2:
-            facturaPlato = "Pizza Queso";
-            total += 3.50;
-            break;
-          default:
-            facturaPlato = "Pizza Combinación";
-            // total += 8.50
-            break;
-        }
-        break;
+      // 2. ACOMPAÑAMIENTO
+      System.out.println("\n--- ACOMPAÑAMIENTOS ADICIONALES ---");
+      System.out.println("1. Papas fritas ($2.50)");
+      System.out.println("2. Patacones ($2.50)");
+      System.out.println("3. Yuca ($2.00)");
+      System.out.println("4. Ninguno");
+      System.out.print("Seleccione extra: ");
+      int extra = sc.nextInt();
+      switch (extra) {
+        case 1:
+          facturaExtra = "Papas fritas";
+          total += 2.50;
+          break;
+        case 2:
+          facturaExtra = "Patacones";
+          total += 2.50;
+          break;
+        case 3:
+          facturaExtra = "Yuca";
+          total += 2.00;
+          break;
+        default:
+          facturaExtra = "Sin adicionales";
+          break;
+      }
 
-      case 3:
-        facturaPlato = "Salchipapas";
-        total += 6.50;
-        break;
-      case 4:
-        facturaPlato = "Hot Dog";
-        total += 2.00;
-        break;
-      case 5:
-        facturaPlato = "Alitas BBQ";
-        total += 8.50;
-        break;
-      case 6:
-        facturaPlato = "Pollo Apanado";
-        total += 3.50;
-        break;
-      case 7:
-        facturaPlato = "Pollo Asado";
-        total += 5.00;
-        break;
-      default:
-        facturaPlato = "Orden Genérica";
-        total += 5.00;
-        break;
-    }
+      // 3. BEBIDAS
+      System.out.println("\n--- BEBIDAS ---");
+      System.out.println("1. Soda ($1.50)");
+      System.out.println("2. Jugo Natural ($3.00)");
+      System.out.println("3. Limonada ($2.50)");
+      System.out.println("4. agua");
+      System.out.println("5. ninguna");
+      System.out.print("Seleccione bebida: ");
+      int bebida = sc.nextInt();
 
-    // 2. ACOMPAÑAMIENTO
-    System.out.println("\n--- ACOMPAÑAMIENTOS ADICIONALES ---");
-    System.out.println("1. Papas fritas ($2.50)");
-    System.out.println("2. Patacones ($2.50)");
-    System.out.println("3. Yuca ($2.00)");
-    System.out.println("4. Ninguno");
-    System.out.print("Seleccione extra: ");
-    int extra = sc.nextInt();
-    switch (extra) {
-      case 1:
-        facturaExtra = "Papas fritas";
-        total += 2.50;
-        break;
-      case 2:
-        facturaExtra = "Patacones";
-        total += 2.50;
-        break;
-      case 3:
-        facturaExtra = "Yuca";
-        total += 2.00;
-        break;
-      default:
-        facturaExtra = "Sin adicionales";
-        break;
-    }
+      switch (bebida) {
+        case 1: // SODA
+          System.out.print("Sabor: (1) Coca Cola, (2) Pepsi, (3) Fresca: ");
+          int tipoS = sc.nextInt();
+          facturaBebida = (tipoS == 1 ? "Coca Cola" : (tipoS == 2 ? "Pepsi" : "Fresca"));
+          total += 1.50;
+          break;
+        case 2: // JUGO
+          System.out.print("Sabor: (1) Guayaba, (2) Piña, (3) Frutos Rojos, (4) Naranja: ");
+          int tipoJ = sc.nextInt();
+          facturaBebida = "Jugo de Fruta";
+          total += 3.00;
+          break;
+        case 3:
+          facturaBebida = "Limonada";
+          total += 2.50;
+          break;
 
-    // 3. BEBIDAS
-    System.out.println("\n--- BEBIDAS ---");
-    System.out.println("1. Soda ($1.50)");
-    System.out.println("2. Jugo Natural ($3.00)");
-    System.out.println("3. Limonada ($2.50)");
-    System.out.println("4. agua");
-    System.out.println("5. ninguna");
-    System.out.print("Seleccione bebida: ");
-    int bebida = sc.nextInt();
+      }
 
-    switch (bebida) {
-      case 1: // SODA
-        System.out.print("Sabor: (1) Coca Cola, (2) Pepsi, (3) Fresca: ");
-        int tipoS = sc.nextInt();
-        facturaBebida = (tipoS == 1 ? "Coca Cola" : (tipoS == 2 ? "Pepsi" : "Fresca"));
-        total += 1.50;
-        break;
-      case 2: // JUGO
-        System.out.print("Sabor: (1) Guayaba, (2) Piña, (3) Frutos Rojos, (4) Naranja: ");
-        int tipoJ = sc.nextInt();
-        facturaBebida = "Jugo de Fruta";
-        total += 3.00;
-        break;
-      case 3:
-        facturaBebida = "Limonada";
-        total += 2.50;
-        break;
+      // 4. GENERAR FACTURA
+      System.out.println("\n============================================");
+      System.out.println("           FACTURA DE CONSUMO               ");
+      System.out.println("============================================");
+      System.out.println("Plato Principal:  " + facturaPlato);
+      System.out.println("Acompañamiento:   " + facturaExtra);
+      System.out.println("Bebida:           " + facturaBebida);
+      System.out.println("--------------------------------------------");
+      System.out.printf("TOTAL A PAGAR:    $%.2f\n", total);
+      System.out.println("============================================");
+      System.out.println("   Gracias por preferir El Garaje del Sabor ");
+      System.out.println("============================================");
 
-    }
-
-    // 4. GENERAR FACTURA
-    System.out.println("\n============================================");
-    System.out.println("           FACTURA DE CONSUMO               ");
-    System.out.println("============================================");
-    System.out.println("Plato Principal:  " + facturaPlato);
-    System.out.println("Acompañamiento:   " + facturaExtra);
-    System.out.println("Bebida:           " + facturaBebida);
-    System.out.println("--------------------------------------------");
-    System.out.printf("TOTAL A PAGAR:    $%.2f\n", total);
-    System.out.println("============================================");
-    System.out.println("   Gracias por preferir El Garaje del Sabor ");
-    System.out.println("============================================");
+    } while (continuar); // El programa se repite mientras 'continuar' sea verdadero
 
     sc.close();
   }
