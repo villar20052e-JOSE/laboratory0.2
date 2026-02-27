4dd5d33fbbb190e90781b3907d3c068258188573
Ric War <ricardo_guerra05@hotmail.com>
2026-02-27 17:48:02 -0500

    en esta nueva actualizacion de mi codigo le agrege el bucle do while para hacer mi codigo mas dinamico

--- Areyouoflegalage.java
+++ Areyouoflegalage.java
@@ -5,11 +5,13 @@ public class Areyouoflegalage{
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in); //Creamos el objeto Scanner para leer datos desde el teclado
+        String option;
 
+        do {  //ciclo repetitivo 
         System.out.print("Enter your Age"); //pedimos al usuario que ingrese su edad 
         int age = sc.nextInt();
 
-        if (age <0){ //
+        if (age <0){ //estructura condicional(desicition)
             System.out.println("Invalid Age"); //validamos primero si la edad es negativa
         } else if (age>=60){
             System.out.println("you are an older adult ");//luego validamos si es adulto mayor de 60 años o mas 
@@ -18,6 +20,13 @@ public class Areyouoflegalage{
         } else {
             System.out.println("you are underage "); //si no cumple  ninguna de las anteriores en menor de edad 
         }
+
+        System.out.print("Do you want to continue? (yes/no):");
+        option = sc.next();
+
+    } while (option.equalsIgnoreCase("yes")); //condiciones de repetir y equal compara texto sin importar la mayuscula/minsucula
+
+    System.out.println("Program finished.");
         sc.close(); //cerramos el scanner 
     }
 }
