98e5f3e5e8c13daf438ca78bc50d0b30af4219b7
aldricksao-code <aldricksao@gmail.com>
2026-02-27 16:57:07 -0500

    se agregado una consicion mas para que nos dija si estamos cerca del numero

--- GuessNumber.java
+++ GuessNumber.java
@@ -1,19 +1,28 @@
 import java.util.Scanner;
+import java.util.Random;
+
 public class GuessNumber {
     public static void main(String[] args) {
         
         Scanner entrada = new Scanner(System.in);
-        int SecretNumber= 23;
-        int Attemps = 0;
+        Random random = new Random();
 
-        System.out.println("adivinanumero el numerosecreto(1 al 50)");
+        int SecretNumber= random.nextInt(30) + 1;
+
+        int Attemps = 0;
+        int contador = 0;
+
+        System.out.println("adivinanumero el numerosecreto(1 al 30)");
         
         do {
-            System.out.print("ingrsa un numero:");
+            System.out.print("ingresa un numero:");
             Attemps= entrada.nextInt();
+            contador++;
 
             if (Attemps == SecretNumber){
-                System.out.println("el numero es correcto");
+                System.out.println(" ¡Correcto! adivinaste el numero felicidades");
+            }else if  (Math.abs(Attemps - SecretNumber) <= 5) {
+                System.out.println("¡Estás cerca del numero!");
             } else if (Attemps < SecretNumber) {
                 System.out.println("el numero es muy bajo");
             } else {
@@ -21,6 +30,8 @@ public class GuessNumber {
             }
 
         } while(Attemps != SecretNumber);
+
+        System.out.println("intentos realizados:" + contador);
 
         entrada.close();
     }  
