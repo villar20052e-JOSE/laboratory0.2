844a1c5984874865c9dbdcac305b53a0d2b2c7e7
JOSE_VILLAR <villar20052@gmail.com>
2026-02-27 17:58:19 -0500

    este es mi programa de menu de comida, este programa fue hecho utilizando Scanner para la lectura y guarda de info del usuario(de lo que vaya pidiendo de comer), se utilizo tambien el switch para la toma de deciciones dependiendo de lo que el usuario elija, aplico un bucle con el do while para continuidad del programa

--- GuessNumber.java
+++ GuessNumber.java
@@ -1,27 +1,27 @@
-import java.util.Scanner;
 import java.util.Random;
+import java.util.Scanner;
 
 public class GuessNumber {
     public static void main(String[] args) {
-         
+
         Scanner entrada = new Scanner(System.in);
         Random random = new Random();
 
-        int SecretNumber= random.nextInt(30) + 1;
+        int SecretNumber = random.nextInt(30) + 1;
 
         int Attemps = 0;
         int contador = 0;
 
         System.out.println("adivinanumero el numerosecreto(1 al 30)");
-        
+
         do {
             System.out.print("ingresa un numero:");
-            Attemps= entrada.nextInt();
+            Attemps = entrada.nextInt();
             contador++;
 
-            if (Attemps == SecretNumber){
+            if (Attemps == SecretNumber) {
                 System.out.println(" ¡Correcto! adivinaste el numero felicidades");
-            }else if  (Math.abs(Attemps - SecretNumber) <= 5) {
+            } else if (Math.abs(Attemps - SecretNumber) <= 5) {
                 System.out.println("¡Estás cerca del numero!");
             } else if (Attemps < SecretNumber) {
                 System.out.println("el numero es muy bajo");
@@ -29,11 +29,11 @@ public class GuessNumber {
                 System.out.println("el numero es muy alto");
             }
 
-        } while(Attemps != SecretNumber);
+        } while (Attemps != SecretNumber);
 
         System.out.println("intentos realizados:" + contador);
 
         entrada.close();
-    }  
+    }
 
 }
