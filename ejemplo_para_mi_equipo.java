43774b256c5cf63cef7a2b86dc2214979c342ee5
JOSE_VILLAR <villar20052e@gmail.com>
2026-03-05 18:38:28 -0500

    Delete ejemplo_para_mi_equipo.java

--- ejemplo_para_mi_equipo.java
+++
@@ -1,8 +0,0 @@
-public class ejemplo_para_mi_equipo {
-  public static void main(String[] args) {
-    System.out.println("git add . ( prepara los cambios para subir)");
-    System.out.println("git commit -am ( agragar comentario )");
-    System.out.println("git push (subir programa)");
-    System.out.println("git pull (descarga el progreso de tus compañeros)");
-  }
-}
